77a50b2b87fa7d7f5937ee5073e46d0d55482a3c
Adel Amodwala <adel.amodwala@gmail.com>
2017-08-10 12:50:21 -0400

    dev: remove all caches

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,24 +1,13 @@
 package hello;
 
-import hello.config.IgniteConfig;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.Ignition;
-import org.apache.ignite.configuration.IgniteConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(IgniteConfig.class)
 public class Application {
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public Ignite ignite(IgniteConfiguration igniteConfiguration) {
-        return Ignition.start(igniteConfiguration);
-    }
 }
